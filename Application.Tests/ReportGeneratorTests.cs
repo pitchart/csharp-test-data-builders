c2179aa298fc509ffc1edf80a1db6e7a1405857b
Mohammed Sajid <mohammedsajid.ensias@gmail.com>
2021-10-25 11:48:11 +0200

    test non complet.

---
+++ Application.Tests/ReportGeneratorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Application.Purchase;
+using Application.Report;
+using Application.Tests.Storage;
+using Xunit;
+
+namespace Application.Tests
+{
+    public class ReportGeneratorTests
+    {
+        [Fact]
+        public void test()
+        {
+            //Arrange
+            ReportGenerator generator = new ReportGenerator();
+            InMemoryRepository inMemoryRepository = new InMemoryRepository();
+            MainRepository.Override(inMemoryRepository);
+
+            Invoice invoice = new Invoice()
+            {
+                PurchasedBooks =
+                {
+                    new PurchasedBook
+                    {
+
+                    }
+                }
+            };
+            inMemoryRepository.AddInvoice();
+
+            //Act
+            var amount = generator.GetTotalAmount();
+
+            //Assert
+
+            MainRepository.Reset();
+        }
+    }
+}
